4e0a75bf3129ea96e8158be790f88daa2512e858
PercyDv <daviamaral2912@gmail.com>
2025-10-29 14:25:16 +0000

    Criação de caminho java para o código, correção dos erros de import usando Java: Clean Java Language Server Workspace, pacotes definidos em todas as classes

--- ComposicaoEnumeracao/src/model/enums/OrderStatus.java
+++ ComposicaoEnumeracao/src/model/enums/OrderStatus.java
@@ -1,9 +1,8 @@
-package com.example.model.enums;
+package model.enums; 
 
 public enum OrderStatus {
     PENDING_PAYMENT,
     PROCESSING,
     SHIPPED,
-    DELIVERED,
-    CANCELLED
+    DELIVERED;
 }
