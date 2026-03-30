8734ea3295474ace1b603b596db4bb51a7f8d55c
D4V!D <daviamaral2912@gmail.com>
2026-03-30 17:47:45 +0000

    Aplicação do bufferedReader e FileReader, assim como suas contra partes writer

--- trabalhando-com-arquivos/Arquivos/src/buffFileApplication/Program.java
+++ trabalhando-com-arquivos/Arquivos/src/buffFileApplication/Program.java
@@ -1,5 +1,26 @@
 package src.buffFileApplication;
 
-public class Program {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Program {
+    public static void main(String[] args) {
+        
+        String path = "C:\\temp\\in";
+       
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            String line = br.readLine();
+
+            while(line != null){
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: "+e.getMessage());
+        } 
+    
+    }
 }
